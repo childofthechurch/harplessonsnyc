5e9b64baa61359459225044c34145be14b6aec6e
saksoft <lennonbus@MacBook-Pro.local>
2026-01-23 16:29:15 -0500

    Temporarily revert Next.js redesign - restore working HTML site

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // For static export if needed
-  },
-}
-
-module.exports = nextConfig
